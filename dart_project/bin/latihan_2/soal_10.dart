b2ea127af1aa179929bcbe79d542ad35c8d0f544
kayshallvy <kayzaalvy1214@gmail.com>
2026-03-14 22:39:28 +0700

    cetak list panjang kata menggunakan for-in

---
+++ dart_project/bin/latihan_2/soal_10.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<String> kataMutiara = ['Belajar', 'Dart', 'Sangat', 'Menyenangkan'];
+
+  for (String kata in kataMutiara) {
+    print("Kata: '$kata', Panjang: ${kata.length}");
+  }
+}
